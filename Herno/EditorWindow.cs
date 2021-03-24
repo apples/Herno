4747f7b468cd0939219aebbd7bb97c15b89350c7
Thalitech <zbigmoney305@gmail.com>
2021-03-24 16:48:55 -0500

    setup imgui

--- Herno/EditorWindow.cs
+++ Herno/EditorWindow.cs
@@ -1,8 +1,13 @@
-﻿using SharpDX.Direct3D;
+﻿using HernoEditor.ImGui;
+using ImGuiNET;
+using SharpDX;
+using SharpDX.D3DCompiler;
+using SharpDX.Direct3D;
 using SharpDX.DXGI;
 using SharpDX.Windows;
 using System;
 using System.Drawing;
+using System.Linq;
 using System.Reflection;
 using D3D11 = SharpDX.Direct3D11;
 
@@ -11,13 +16,39 @@ namespace HernoEditor
     class EditorWindow : IDisposable
     {
         private RenderForm renderForm;
+
         private const int Width = 1280;
         private const int Height = 720;
 
-        private D3D11.Device d3dDevice;
-        private D3D11.DeviceContext d3dDeviceContext;
+        private D3D11.Device device;
+        private D3D11.DeviceContext context;
         private SwapChain swapChain;
         private D3D11.RenderTargetView renderTargetView;
+        private Viewport viewport;
+
+        // Shaders
+        private D3D11.VertexShader vertexShader;
+        private D3D11.PixelShader pixelShader;
+        private ShaderSignature inputSignature;
+        private D3D11.InputLayout inputLayout;
+        private D3D11.InputElement[] inputElements = new D3D11.InputElement[] { new D3D11.InputElement("POSITION", 0, Format.R32G32B32_Float, 0) };
+        
+        // Triangle vertices
+        private Vector3[] vertices = new Vector3[] { new Vector3(-0.5f, -0.5f, 0.0f), new Vector3(0.0f, 0.5f, 0.0f), new Vector3(0.5f, -0.5f, 0.0f) };
+        private D3D11.Buffer triangleVertexBuffer;
+
+        // UI state
+        private static ImGuiController _controller;
+        private static float _f = 0.0f;
+        private static int _counter = 0;
+        private static int _dragInt = 0;
+        private static Vector3 _clearColor = new Vector3(0.45f, 0.55f, 0.6f);
+        private static bool _showImGuiDemoWindow = true;
+        private static bool _showAnotherWindow = false;
+        private static bool _showMemoryEditor = false;
+        private static uint s_tab_bar_flags = (uint)ImGuiTabBarFlags.Reorderable;
+        static bool[] s_opened = { true, true, true, true }; // Persistent user state
+        static void SetThing(out float i, float val) { i = val; }
 
         /// <summary>
 		/// Create and initialize a new game.
@@ -28,8 +59,11 @@ namespace HernoEditor
             renderForm.ClientSize = new Size(Width, Height);
             renderForm.Icon = Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location);
             renderForm.AllowUserResizing = false;
+            renderForm.WindowState = System.Windows.Forms.FormWindowState.Normal;
             
             InitializeDeviceResources();
+            InitializeShaders();
+            InitializeTriangle();
         }
 
         /// <summary>
@@ -45,6 +79,33 @@ namespace HernoEditor
             Draw();
         }
 
+        private void InitializeShaders()
+        {
+            // Compile the vertex shader code
+            using (var vertexShaderByteCode = ShaderBytecode.CompileFromFile("Resources/Shaders/DefaultShader.shader", "vertex", "vs_4_0", ShaderFlags.Debug))
+            {
+                // Read input signature from shader code
+                inputSignature = ShaderSignature.GetInputSignature(vertexShaderByteCode);
+                vertexShader = new D3D11.VertexShader(device, vertexShaderByteCode);
+            }
+            // Compile the pixel shader code
+            using (var pixelShaderByteCode = ShaderBytecode.CompileFromFile("Resources/Shaders/DefaultShader.shader", "pixel", "ps_4_0", ShaderFlags.Debug))
+            {
+                pixelShader = new D3D11.PixelShader(device, pixelShaderByteCode);
+            }
+
+            // Set as current vertex and pixel shaders
+            context.VertexShader.Set(vertexShader);
+            context.PixelShader.Set(pixelShader);
+
+            context.InputAssembler.PrimitiveTopology = PrimitiveTopology.TriangleList;
+
+            // Create the input layout from the input signature and the input elements
+            inputLayout = new D3D11.InputLayout(device, inputSignature, inputElements);
+
+            // Set input layout to use
+            context.InputAssembler.InputLayout = inputLayout;
+        }
 
         private void InitializeDeviceResources()
         {
@@ -62,14 +123,23 @@ namespace HernoEditor
             };
 
             // Create device and swap chain
-            D3D11.Device.CreateWithSwapChain(DriverType.Hardware, D3D11.DeviceCreationFlags.None, swapChainDesc, out d3dDevice, out swapChain);
-            d3dDeviceContext = d3dDevice.ImmediateContext;
+            D3D11.Device.CreateWithSwapChain(DriverType.Hardware, D3D11.DeviceCreationFlags.None, swapChainDesc, out device, out swapChain);
+            context = device.ImmediateContext;
+
+            viewport = new Viewport(0, 0, Width, Height);
+            context.Rasterizer.SetViewport(viewport);
 
             // Create render target view for back buffer
             using (D3D11.Texture2D backBuffer = swapChain.GetBackBuffer<D3D11.Texture2D>(0))
             {
-                renderTargetView = new D3D11.RenderTargetView(d3dDevice, backBuffer);
+                renderTargetView = new D3D11.RenderTargetView(device, backBuffer);
             }
+        }
+        
+        private void InitializeTriangle()
+        {
+            // Create a vertex buffer, and use our array with vertices as data
+            triangleVertexBuffer = D3D11.Buffer.Create(device, D3D11.BindFlags.VertexBuffer, vertices);
         }
 
         /// <summary>
@@ -78,21 +148,159 @@ namespace HernoEditor
 		private void Draw()
         {
             // Set back buffer as current render target view
-            d3dDeviceContext.OutputMerger.SetRenderTargets(renderTargetView);
+            context.OutputMerger.SetRenderTargets(renderTargetView);
 
             // Clear the screen
-            d3dDeviceContext.ClearRenderTargetView(renderTargetView, new SharpDX.Color(40, 40, 40));
+            context.ClearRenderTargetView(renderTargetView, new SharpDX.Color(40, 40, 40));
+
+            DrawImGui();
+
+            // Set vertex buffer
+            context.InputAssembler.SetVertexBuffers(0, new D3D11.VertexBufferBinding(triangleVertexBuffer, Utilities.SizeOf<Vector3>(), 0));
+            // Draw the triangle
+            context.Draw(vertices.Count(), 0);
 
             // Swap front and back buffer
             swapChain.Present(1, PresentFlags.None);
         }
 
+        private void DrawImGui()
+        {
+            renderForm.Resize += (a, b) =>
+            {
+                swapChain.ResizeBuffers(0, (int)renderForm.Width, (int)renderForm.Height, Format.R8G8B8A8_UNorm, SwapChainFlags.None);
+                _controller.WindowResized(renderForm.Width, renderForm.Height);
+            };
+
+
+
+            // Demo code adapted from the official Dear ImGui demo program:
+            // https://github.com/ocornut/imgui/blob/master/examples/example_win32_directx11/main.cpp#L172
+
+            // 1. Show a simple window.
+            // Tip: if we don't call ImGui.BeginWindow()/ImGui.EndWindow() the widgets automatically appears in a window called "Debug".
+            {
+                ImGui.Text("Hello, world!");                                        // Display some text (you can use a format string too)
+                ImGui.SliderFloat("float", ref _f, 0, 1, _f.ToString("0.000"));  // Edit 1 float using a slider from 0.0f to 1.0f    
+                //ImGui.ColorEdit3("clear color", ref _clearColor);                   // Edit 3 floats representing a color
+
+                ImGui.Text($"Mouse position: {ImGui.GetMousePos()}");
+
+                ImGui.Checkbox("ImGui Demo Window", ref _showImGuiDemoWindow);                 // Edit bools storing our windows open/close state
+                ImGui.Checkbox("Another Window", ref _showAnotherWindow);
+                ImGui.Checkbox("Memory Editor", ref _showMemoryEditor);
+                if (ImGui.Button("Button"))                                         // Buttons return true when clicked (NB: most widgets return true when edited/activated)
+                    _counter++;
+                ImGui.SameLine(0, -1);
+                ImGui.Text($"counter = {_counter}");
+
+                ImGui.DragInt("Draggable Int", ref _dragInt);
+
+                float framerate = ImGui.GetIO().Framerate;
+                ImGui.Text($"Application average {1000.0f / framerate:0.##} ms/frame ({framerate:0.#} FPS)");
+            }
+
+            // 2. Show another simple window. In most cases you will use an explicit Begin/End pair to name your windows.
+            if (_showAnotherWindow)
+            {
+                ImGui.Begin("Another Window", ref _showAnotherWindow);
+                ImGui.Text("Hello from another window!");
+                if (ImGui.Button("Close Me"))
+                    _showAnotherWindow = false;
+                ImGui.End();
+            }
+
+            // 3. Show the ImGui demo window. Most of the sample code is in ImGui.ShowDemoWindow(). Read its code to learn more about Dear ImGui!
+            if (_showImGuiDemoWindow)
+            {
+                // Normally user code doesn't need/want to call this because positions are saved in .ini file anyway.
+                // Here we just want to make the demo initial state a bit more friendly!
+                ImGui.SetNextWindowPos(new System.Numerics.Vector2(650, 20), ImGuiCond.FirstUseEver);
+                ImGui.ShowDemoWindow(ref _showImGuiDemoWindow);
+            }
+
+            if (ImGui.TreeNode("Tabs"))
+            {
+                if (ImGui.TreeNode("Basic"))
+                {
+                    ImGuiTabBarFlags tab_bar_flags = ImGuiTabBarFlags.None;
+                    if (ImGui.BeginTabBar("MyTabBar", tab_bar_flags))
+                    {
+                        if (ImGui.BeginTabItem("Avocado"))
+                        {
+                            ImGui.Text("This is the Avocado tab!\nblah blah blah blah blah");
+                            ImGui.EndTabItem();
+                        }
+                        if (ImGui.BeginTabItem("Broccoli"))
+                        {
+                            ImGui.Text("This is the Broccoli tab!\nblah blah blah blah blah");
+                            ImGui.EndTabItem();
+                        }
+                        if (ImGui.BeginTabItem("Cucumber"))
+                        {
+                            ImGui.Text("This is the Cucumber tab!\nblah blah blah blah blah");
+                            ImGui.EndTabItem();
+                        }
+                        ImGui.EndTabBar();
+                    }
+                    ImGui.Separator();
+                    ImGui.TreePop();
+                }
+
+                if (ImGui.TreeNode("Advanced & Close Button"))
+                {
+                    // Expose a couple of the available flags. In most cases you may just call BeginTabBar() with no flags (0).
+                    ImGui.CheckboxFlags("ImGuiTabBarFlags_Reorderable", ref s_tab_bar_flags, (uint)ImGuiTabBarFlags.Reorderable);
+                    ImGui.CheckboxFlags("ImGuiTabBarFlags_AutoSelectNewTabs", ref s_tab_bar_flags, (uint)ImGuiTabBarFlags.AutoSelectNewTabs);
+                    ImGui.CheckboxFlags("ImGuiTabBarFlags_NoCloseWithMiddleMouseButton", ref s_tab_bar_flags, (uint)ImGuiTabBarFlags.NoCloseWithMiddleMouseButton);
+                    if ((s_tab_bar_flags & (uint)ImGuiTabBarFlags.FittingPolicyMask) == 0)
+                        s_tab_bar_flags |= (uint)ImGuiTabBarFlags.FittingPolicyDefault;
+                    if (ImGui.CheckboxFlags("ImGuiTabBarFlags_FittingPolicyResizeDown", ref s_tab_bar_flags, (uint)ImGuiTabBarFlags.FittingPolicyResizeDown))
+                        s_tab_bar_flags &= ~((uint)ImGuiTabBarFlags.FittingPolicyMask ^ (uint)ImGuiTabBarFlags.FittingPolicyResizeDown);
+                    if (ImGui.CheckboxFlags("ImGuiTabBarFlags_FittingPolicyScroll", ref s_tab_bar_flags, (uint)ImGuiTabBarFlags.FittingPolicyScroll))
+                        s_tab_bar_flags &= ~((uint)ImGuiTabBarFlags.FittingPolicyMask ^ (uint)ImGuiTabBarFlags.FittingPolicyScroll);
+
+                    // Tab Bar
+                    string[] names = { "Artichoke", "Beetroot", "Celery", "Daikon" };
+
+                    for (int n = 0; n < s_opened.Length; n++)
+                    {
+                        if (n > 0) { ImGui.SameLine(); }
+                        ImGui.Checkbox(names[n], ref s_opened[n]);
+                    }
+
+                    // Passing a bool* to BeginTabItem() is similar to passing one to Begin(): the underlying bool will be set to false when the tab is closed.
+                    if (ImGui.BeginTabBar("MyTabBar", (ImGuiTabBarFlags)s_tab_bar_flags))
+                    {
+                        for (int n = 0; n < s_opened.Length; n++)
+                            if (s_opened[n] && ImGui.BeginTabItem(names[n], ref s_opened[n]))
+                            {
+                                ImGui.Text($"This is the {names[n]} tab!");
+                                if ((n & 1) != 0)
+                                    ImGui.Text("I am an odd tab.");
+                                ImGui.EndTabItem();
+                            }
+                        ImGui.EndTabBar();
+                    }
+                    ImGui.Separator();
+                    ImGui.TreePop();
+                }
+                ImGui.TreePop();
+            }
+
+            ImGuiIOPtr io = ImGui.GetIO();
+            SetThing(out io.DeltaTime, 2f);
+        }
+
         public void Dispose()
         {
+            triangleVertexBuffer.Dispose();
+            vertexShader.Dispose();
+            pixelShader.Dispose();
             renderTargetView.Dispose();
             swapChain.Dispose();
-            d3dDevice.Dispose();
-            d3dDeviceContext.Dispose();
+            device.Dispose();
+            context.Dispose();
             renderForm.Dispose();
         }
     }
